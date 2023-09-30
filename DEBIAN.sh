b2ecf0da1afbe09cf55a4e39f76f1e6852224280
OleksiiJS <alexey.kriachok@gmail.com>
2023-10-01 02:41:14 +0300

    @1

---
+++ DEBIAN.sh
@@ -0,0 +1,7 @@
+#   sudo su 
+#   sudo apt-get update
+#   sudo apt install git 
+#   git clone https://github.com/OleksiiJS-dev/LUNIX_DEBIAN11.git
+#   cd LUNIX_DEBIAN11/DEBIAN.sh
+
+sudo apt-get install apache2 php7.0
