b77f81c14fa1e32261a04b6356a3f2de4ea44a0e
OleksiiJS <alexey.kriachok@gmail.com>
2023-10-01 13:36:31 +0300

    00

--- bash.sh
+++ bash.sh
@@ -1,11 +1,8 @@
+#!/bin/bash
 sudo su 
-#   github link to project
-# git clone https://github.com/OleksiiJS-dev/LUNIX_DEBIAN11.git
-# bash LUNIX_DEBIAN11/DEBIAN.sh
+#APACHE
 sudo apt-get install apache2 php7.0
-sudo apt-get install npm
-npm install -g pm2
-echo '<VirtualHost *:80>
+echo    '<VirtualHost *:80>
         ServerAdmin webmaster@localhost
         ServerName http://lamborghini-aventador.pp.ua/
         DocumentRoot /var/www/html
@@ -13,8 +10,14 @@ echo '<VirtualHost *:80>
         ProxyPassReverse / http://localhost:3000/
         ErrorLog ${APACHE_LOG_DIR}/error.log
         CustomLog ${APACHE_LOG_DIR}/access.log combined
-</VirtualHost>
-' | sudo tee /etc/apache2/sites-available/000-default.conf 
+        </VirtualHost>
+        ' | sudo tee /etc/apache2/sites-available/000-default.conf 
 sudo a2enmod proxy
 sudo a2enmod proxy_http
 sudo systemctl restart apache2
+#GIT APLICA
+#   github link to project
+# git clone https://github.com/OleksiiJS-dev/LUNIX_DEBIAN11.git
+# bash LUNIX_DEBIAN11/DEBIAN.sh
+sudo apt-get install npm
+npm install -g pm2
